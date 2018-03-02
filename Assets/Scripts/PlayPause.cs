5ad9da90e9eff96a59541e5698e0bb1e486b7585
CAMPUS\hollowed17 <danhollowell@gmail.com>
2018-03-02 15:54:44 -0800

    Initial Commit of App

---
+++ Assets/Scripts/PlayPause.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayPause : MonoBehaviour {
+    public AudioSource source;
+
+    public void TogglePlay()
+    {
+        if (source.isPlaying)
+        {
+            source.Stop();
+        }
+        else
+        {
+            source.Play();
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/VolumeSliderController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSliderController : MonoBehaviour {
+    public Slider volumeSlider;
+    public AudioSource[] volumeAudio;
+    public void OnValueChanged()
+    {
+        for (int i =  0; i< volumeAudio.Length; i++)
+        {
+            volumeAudio[i].volume = volumeSlider.value ;
+        }
+        
+    }
+}
